c187ba6af8387443a2aac01a033d71b22c9f3ae7
AISSAOUI Anis <aissaoui.anis35@gmail.com>
2023-02-24 17:25:32 +0100

    init v3

---
+++ src/components/Layout.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Layout({children}) {
+  return (
+    <div>
+        <nav>
+            hello world
+        </nav>
+        {children}
+    </div>
+  )
+}
