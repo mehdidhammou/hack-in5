afcdcee343306f7e075143b4914ab8ebf8dcfd1e
mehdidhammou <mehdidhammou@protonmail.com>
2023-02-25 06:20:13 +0100

    landing page

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 
 export default function Hero() {
-    const variable = 'Hero';
   return (
-    <div>
-      navbar
+    <div className='flex flex-col md:flex-row items-center justify-center min-h-screen'>
+      <h1 className='text-5xl'>
+        Hello world
+      </h1>
     </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  darkMode: 'class',
+  content: [
+    "./src/**/*.{js,jsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        'primary': '#0567F0',
+      },
+    },
   },
   plugins: [
-    require('daisyui'),
   ],
 }
